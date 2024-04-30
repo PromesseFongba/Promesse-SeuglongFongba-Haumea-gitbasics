48a0c2da182bf5ee5ce70a4fc57d3185f9cd9dda
unknown <PromesseFongba>
2024-04-29 20:54:21 -0400

    skills update

--- Js/index.js
+++ Js/index.js
@@ -8,17 +8,20 @@ footer.innerHTML = `&copy; ${thisYear} Promesse Fongba. All rights reserved.`;
 document.body.appendChild(footer); // append footer to body
 
 // Step 3: List your technical skills in an array
-let skills = ["JavaScript", "HTML", "CSS", "Adobe Photoshop", "GitHub"];
-
-// Step 4: Select the section of the document where you want to display the skills
-let skillsSection = document.querySelector("#skills");
-
-// Step 5: Select the unordered list within the skills section
-let skillsList = skillsSection.querySelector("ul");
-
-// Step 6: Loop over each skill in the array
-skills.forEach(skill => {
-    let listItem = document.createElement("li"); // create a list item for each skill
-    listItem.innerText = skill;
-    skillsList.appendChild(listItem); // append each skill to the list
-});
+let skills = [
+    { main: "UI/UX Design", sub: "Adobe Illustrator, Canva, Adobe Photoshop" },
+    { main: "Software Engineering", sub: "HTML5, CSS3, Javascript, python, SQL" },
+    {
+      main: "Soft Skills",
+      sub: "Bilingual(French/English), persistent, ambitous, team player, emphatetic",
+    },
+   
+  ];
+  
+  let container = document.querySelector(".skills_container");
+  skills.forEach((skill) => {
+    let el = document.createElement("article");
+    el.className = "card";
+    el.innerHTML = `<h3>${skill.main}</h3><p>${skill.sub}</p>`;
+    container.appendChild(el);
+  });
