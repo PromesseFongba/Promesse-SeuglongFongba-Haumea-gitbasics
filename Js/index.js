64f5ce295d4acb5f4a2503fd28f97a67e57b8016
unknown <PromesseFongba>
2024-05-27 15:16:02 -0400

    Final project

--- Js/index.js
+++ Js/index.js
@@ -1,3 +1,10 @@
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+
 const today = new Date();
 const currentYear = today.getFullYear();
 const footer = document.createElement("footer");
@@ -5,19 +12,47 @@ footer.innerHTML = `&copy; ${currentYear} Promesse Fongba. All rights reserved.`
 document.body.appendChild(footer);
 const skills = [
   { main: "UI/UX Design", sub: "Adobe Illustrator, Canva, Adobe Photoshop" },
-  { main: "Software Engineering", sub: "HTML5, CSS3, Javascript, python, SQL" },
+  { main: "Software Engineering", sub: "HTML5, CSS3, Javascript, Python, SQL" },
   {
     main: "Soft Skills",
-    sub: "Bilingual(French/English), persistent, ambitous, team player",
+    sub: "Bilingual(French/English), persistent, ambitious, team player",
+    audio: true
   },
 ];
+
 const container = document.querySelector(".skills_container");
+
 skills.forEach((skill) => {
   const el = document.createElement("article");
   el.className = "card";
+  el.style.width = '500px';  // Set the width of the card
+  el.style.height = '300px';  
+  el.style.display = 'flex';
+  el.style.borderRadius = '20px';
+  el.style.flexDirection = 'column';
+  el.style.backgroundColor = '#d7377a';  
+  el.style.padding = '20px';  
+  el.style.boxShadow = '0 4px 8px rgba(0,0,0,0.1)';  
+
   el.innerHTML = `<h3>${skill.main}</h3><p>${skill.sub}</p>`;
+
+  if (skill.audio) {
+    const audioHtml = `
+      <audio controls="controls">
+        <source src="bilingual.mp3" type="audio/mpeg">
+        <source src="Bilingual.ogg" type="audio/ogg">
+        <a href="bilingual.mp3">Download music from Greg</a> (MP3)
+      </audio>
+    `;
+    el.innerHTML += audioHtml;
+  }
+
   container.appendChild(el);
 });
+
+
+
+
 const createButton = (text) => {
   const btn = document.createElement("button");
   btn.innerText = text;
